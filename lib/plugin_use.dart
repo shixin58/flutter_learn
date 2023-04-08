81ecfd08ce062296cdf5f736a7a37fd010cf35f4
shixin <shixin1359@gmail.com>
2023-04-08 21:29:16 +0800

    引入path_provider插件，打印常见目录

--- lib/plugin_use.dart
+++ lib/plugin_use.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_color_plugin/flutter_color_plugin.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PluginUse extends StatefulWidget {
   const PluginUse({super.key});
@@ -9,6 +12,42 @@ class PluginUse extends StatefulWidget {
 }
 
 class _PluginUseState extends State<PluginUse> {
+  Future<Directory?>? _tempDirectory;
+  Future<Directory?>? _appSupportDirectory;
+  Future<Directory?>? _appDocumentsDirectory;
+
+  void _requestTempDirectory() {
+    setState(() {
+      _tempDirectory = getTemporaryDirectory();
+    });
+  }
+
+  void _requestAppSupportDirectory() {
+    setState(() {
+      _appSupportDirectory = getApplicationSupportDirectory();
+    });
+  }
+
+  void _requestAppDocumentsDirectory() {
+    setState(() {
+      _appDocumentsDirectory = getApplicationDocumentsDirectory();
+    });
+  }
+
+  Widget _buildDirectory(BuildContext context, AsyncSnapshot<Directory?> snapshot) {
+    Text text = const Text('');
+    if (snapshot.connectionState == ConnectionState.done) {
+      if (snapshot.hasError) {
+        text = Text('Error: ${snapshot.error}');
+      } else if (snapshot.hasData) {
+        text = Text('path: ${snapshot.data!.path}');
+      } else {
+        text = const Text('path unavailable');
+      }
+    }
+    return Padding(padding: const EdgeInsets.all(16.0), child: text,);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +67,39 @@ class _PluginUseState extends State<PluginUse> {
             Text(
               'You have pushed the button this many times:',
               style: TextStyle(color: ColorUtil.color('#a9ee00')),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: _requestTempDirectory,
+                child: const Text('Get Temporary Directory'),
+              ),
+            ),
+            FutureBuilder<Directory?>(
+              future: _tempDirectory,
+              builder: _buildDirectory,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: _requestAppSupportDirectory,
+                child: const Text('Get Application Support Directory'),
+              ),
+            ),
+            FutureBuilder<Directory?>(
+              future: _appSupportDirectory,
+              builder: _buildDirectory,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: _requestAppDocumentsDirectory,
+                child: const Text('Get Application Documents Directory'),
+              ),
+            ),
+            FutureBuilder<Directory?>(
+              future: _appDocumentsDirectory,
+              builder: _buildDirectory,
             ),
           ],
         ),
